fa50e49c28b857c763a6657a3dad2bee0566eead
MjesusD <manueladuartetoro04@gmail.com>
2025-12-01 23:51:27 -0300

    Puzzle1 memoria nivel 2

---
+++ Proyecto2/Assets/Scripts/Nivel2/Card.cs
@@ -0,0 +1,55 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Card : MonoBehaviour
+{
+    [Header("Referencias")]
+    public Image frontImage;
+    public Image backImage;
+
+    [Header("Datos")]
+    public int cardId;
+    public bool isRevealed = false;
+    public bool isMatched = false;
+
+    private Button button;
+
+    private void Awake()
+    {
+        button = GetComponent<Button>();
+    }
+
+    public void Setup(int id, Sprite frontSprite)
+    {
+        cardId = id;
+        frontImage.sprite = frontSprite;
+        Hide();
+    }
+
+    // Se llama desde el OnClick del Button
+    public void OnCardClicked()
+    {
+        if (isMatched || isRevealed) return;
+        MemoryGameManager.Instance.SelectCard(this);
+    }
+
+    public void Reveal()
+    {
+        isRevealed = true;
+        frontImage.gameObject.SetActive(true);
+        backImage.gameObject.SetActive(false);
+    }
+
+    public void Hide()
+    {
+        isRevealed = false;
+        frontImage.gameObject.SetActive(false);
+        backImage.gameObject.SetActive(true);
+    }
+
+    public void Lock()
+    {
+        isMatched = true;
+        button.interactable = false;
+    }
+}
---
+++ Proyecto2/Assets/Scripts/Nivel2/MemoryGameManager.cs
@@ -0,0 +1,94 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class MemoryGameManager : MonoBehaviour
+{
+    public static MemoryGameManager Instance;
+
+    [Header("Configuración")]
+    public Transform gridParent;         // El GridLayoutGroup
+    public Card cardPrefab;              // Prefab de la carta
+    public List<Sprite> cardSprites;     // Lista de sprites únicos (uno por pareja)
+    public int pairs = 4;                // Cantidad de pares
+
+    private List<Card> cards = new List<Card>();
+    private Card firstCard;
+    private Card secondCard;
+    private bool canClick = true;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    private void Start()
+    {
+        GenerateCards();
+    }
+
+    void GenerateCards()
+    {
+        List<int> ids = new List<int>();
+
+        // Crear 2 copias de cada ID (pares)
+        for (int i = 0; i < pairs; i++)
+        {
+            ids.Add(i);
+            ids.Add(i);
+        }
+
+        // Mezclar la lista
+        for (int i = 0; i < ids.Count; i++)
+        {
+            int rnd = Random.Range(0, ids.Count);
+            (ids[i], ids[rnd]) = (ids[rnd], ids[i]);
+        }
+
+        // Instanciar cartas según el ID mezclado
+        foreach (int id in ids)
+        {
+            Card newCard = Instantiate(cardPrefab, gridParent);
+            newCard.Setup(id, cardSprites[id]);
+            cards.Add(newCard);
+        }
+    }
+
+    public void SelectCard(Card card)
+    {
+        if (!canClick) return;
+
+        card.Reveal();
+
+        if (firstCard == null)
+        {
+            firstCard = card;
+        }
+        else
+        {
+            secondCard = card;
+            canClick = false;
+            StartCoroutine(CheckPair());
+        }
+    }
+
+    IEnumerator CheckPair()
+    {
+        yield return new WaitForSeconds(0.7f);
+
+        if (firstCard.cardId == secondCard.cardId)
+        {
+            firstCard.Lock();
+            secondCard.Lock();
+        }
+        else
+        {
+            firstCard.Hide();
+            secondCard.Hide();
+        }
+
+        firstCard = null;
+        secondCard = null;
+        canClick = true;
+    }
+}
